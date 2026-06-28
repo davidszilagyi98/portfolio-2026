38b109662a6713907f1c2a2563c0b904c43f616d
davidszilagyi98 <73821029+davidszilagyi98@users.noreply.github.com>
2026-06-28 19:00:24 +0200

    Updated projects-data.ts

--- lib/projects-data.ts
+++ lib/projects-data.ts
@@ -136,5 +136,6 @@ export const projectsData: Project[] = [
     size: "medium",
     metrics: { speed: "0.8s", lighthouse: "95+" },
     metricLabelsKey: { speed: "projects_speed", lighthouse: "Lighthouse" },
+    figmaUrl: "https://www.figma.com/proto/iYPULAw6CABVoL0s1ngio3/Untitled?page-id=0%3A1&type=design&node-id=60-1465&viewport=552%2C148%2C0.04&t=Wl4VxUGWOORAXIHH-1&scaling=min-zoom&starting-point-node-id=60%3A1465&show-proto-sidebar=1"
   },
 ];
